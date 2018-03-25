e2ef824f971f65fece4f0bc442b006600911ac55
VasiliyIsaichkin <vasiliy@isaichkin.ru>
2018-03-25 05:47:24 +0300

    Split constructor in WebApi and WebApiServer

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ class WebApi extends Events {
 
 		this.isServer = isServer;
 
+		this._initOptions();
+
+		this.socket = socket;
+		this.socketEvents = new Events();
+
+		this.use((...args) => this.cmdHandler(...args));
+		this.socket.on(this.options.cmdToTargetRet, (packet) => this.cmdReturnHandler(packet));
+
+		this.socket.on(this.options.cmdFromTarget, (packet) => this.incomingCmdHandler(packet));
+		this.useAfter((...args) => this.incomingCmdReturnHandler(...args));
+	}
+
+	_initOptions() {
 		const constructEventName = (_isServer, isCmd) =>
 			`${this.options.prefix}|${_isServer ? 'server' : 'client'}|${isCmd ? this.options.eventCmd : this.options.eventCmdRet}`;
 
@@ -23,46 +36,43 @@ class WebApi extends Events {
 			cmdFromTarget   : constructEventName(!isServer, true),
 			cmdFromTargetRet: constructEventName(!isServer, false)
 		});
+	}
 
-		this.socket = socket;
-		this.socketEvents = new Events();
+	async incomingCmdHandler(packet) {
+		if (!packet.name || !tools.isGUID(packet.id) || packet.version !== this.options.version) return;
+		await this.emitEx(packet.name, true, {skipWebApiHandler: true, webApiPacketId: packet.id}, ...(tools.isArray(packet.args) ? packet.args : [packet.args]));
+	}
 
-		this.socket.on(this.options.cmdToTargetRet, (packet) => {
-			if (!tools.isGUID(packet.id) && packet.version !== this.options.version) return;
-			this.socketEvents.emit(packet.id, packet.args);
-		});
+	cmdReturnHandler(packet) {
+		if (!tools.isGUID(packet.id) && packet.version !== this.options.version) return;
+		this.socketEvents.emit(packet.id, packet.args);
+	}
 
-		this.use((name, options, ...args) => {
-			if (options.skipWebApiHandler) return tools.nop;
-			const id = tools.GUID();
-			const promise = new Promise((resolve) => {
-				//@todo: add garbage collecotor here (delete unused calls after timeout)
-				this.socketEvents.once(id, (ret) => {
-					resolve({ret});
-				});
+	cmdHandler(name, options, ...args) {
+		if (options.skipWebApiHandler) return tools.nop;
+		const id = tools.GUID();
+		const promise = new Promise((resolve) => {
+			//@todo: add garbage collecotor here (delete unused calls after timeout)
+			this.socketEvents.once(id, (ret) => {
+				resolve({ret});
 			});
-
-			this.socket.emit(this.options.cmdToTarget, {
-				id,
-				name,
-				args,
-				version: this.options.version
-			});
-			return promise;
 		});
 
-		this.socket.on(this.options.cmdFromTarget, async (packet) => {
-			if (!packet.name || !tools.isGUID(packet.id) || packet.version !== this.options.version) return;
-			await this.emitEx(packet.name, true, {skipWebApiHandler: true, webApiPacketId: packet.id}, ...(tools.isArray(packet.args) ? packet.args : [packet.args]));
+		this.socket.emit(this.options.cmdToTarget, {
+			id,
+			name,
+			args,
+			version: this.options.version
 		});
+		return promise;
+	}
 
-		this.useAfter((name, mwConfig, ret) => {
-			if (!tools.isObject(ret) || !mwConfig.webApiPacketId) return;
-			const args = Object.keys(ret).length === 1
-			             ? ret[Object.keys(ret)[0]]
-			             : tools.objectToArray(ret);
-			this.socket.emit(this.options.cmdFromTargetRet, {name, id: mwConfig.webApiPacketId, args, version: this.options.version});
-		});
+	incomingCmdReturnHandler(name, mwConfig, ret) {
+		if (!tools.isObject(ret) || !mwConfig.webApiPacketId) return;
+		const args = Object.keys(ret).length === 1
+		             ? ret[Object.keys(ret)[0]]
+		             : tools.objectToArray(ret);
+		this.socket.emit(this.options.cmdFromTargetRet, {name, id: mwConfig.webApiPacketId, args, version: this.options.version});
 	}
 }
 
@@ -74,24 +84,26 @@ class WebApiServer extends Events {
 		}, options);
 
 		this.handlers = {};
-		this.use(async (name, options, ...args) => {
-			let promises = [];
-			if (options.fromMapper) return;
-			tools.iterate(this.handlers, (handler, hid) => {
-				promises.push(new Promise(async (resolve) => {
-					setTimeout(() => resolve({timeout: true, hid}), this.options.emitTimeout);
-					resolve({ret: await handler.emit(name, ...args), timeout: false, hid});
-				}));
-			});
-			const ret = tools.iterate(await Promise.all(promises), (row, _, iter) => {
-				iter.key(row.hid);
-				return row.timeout ? null : row.ret;
-			}, {});
-			return {ret};
-		});
+		this.use((...args) => this.emitHandler(...args));
 
 		io.on('connection', (socket) => this.registerHandler(socket));
 	};
+
+	async emitHandler(name, options, ...args) {
+		let promises = [];
+		if (options.fromMapper) return;
+		tools.iterate(this.handlers, (handler, hid) => {
+			promises.push(new Promise(async (resolve) => {
+				setTimeout(() => resolve({timeout: true, hid}), this.options.emitTimeout);
+				resolve({ret: await handler.emit(name, ...args), timeout: false, hid});
+			}));
+		});
+		const ret = tools.iterate(await Promise.all(promises), (row, _, iter) => {
+			iter.key(row.hid);
+			return row.timeout ? null : row.ret;
+		}, {});
+		return {ret};
+	}
 
 	registerHandler(socket) {
 		socket.on('disconnect', () => this.unRegisterHandelr(socket));
